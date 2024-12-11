7786dc1cfb5325199bf43da33b5955a6afb507df
brucewayne <rahmathulalif@gmail.com>
2024-12-11 11:51:50 +0700

    inisiasi project grid layout 234311030

---
+++ app/src/main/java/com/example/courses/data/DataSource.kt
@@ -0,0 +1,35 @@
+package com.example.courses.data
+
+import com.example.courses.R
+import com.example.courses.model.Topic
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.brimstone, 1, R.drawable.brimstone),
+        Topic(R.string.phoenix, 2, R.drawable.phoenix),
+        Topic(R.string.sage, 3, R.drawable.sage),
+        Topic(R.string.sova, 4, R.drawable.sova),
+        Topic(R.string.viper, 5, R.drawable.viper),
+        Topic(R.string.cypher, 6, R.drawable.cypher),
+        Topic(R.string.omen, 7, R.drawable.omen),
+        Topic(R.string.jett, 8, R.drawable.jett),
+        Topic(R.string.breach, 9, R.drawable.breach),
+        Topic(R.string.raze, 10, R.drawable.raze),
+        Topic(R.string.reyna, 11, R.drawable.reyna),
+        Topic(R.string.killjoy, 12, R.drawable.killjoy),
+        Topic(R.string.skye, 13, R.drawable.skye),
+        Topic(R.string.yoru, 14, R.drawable.yoru),
+        Topic(R.string.astra, 15, R.drawable.astra),
+        Topic(R.string.kayo, 16, R.drawable.kayo),
+        Topic(R.string.chamber, 17, R.drawable.chamber),
+        Topic(R.string.neon, 18, R.drawable.neon),
+        Topic(R.string.fade, 19, R.drawable.fade),
+        Topic(R.string.harbor, 20, R.drawable.harbor),
+        Topic(R.string.gekko, 21, R.drawable.gekko),
+        Topic(R.string.deadlock, 22, R.drawable.deadlock),
+        Topic(R.string.iso, 23, R.drawable.iso),
+        Topic(R.string.clove, 24, R.drawable.clove),
+        Topic(R.string.vyse, 25, R.drawable.vyse),
+
+        )
+}
